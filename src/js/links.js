d216d40bf1c107f41e261425d64a36a1d5fb55e3
z <113864220+bielwtf@users.noreply.github.com>
2023-09-03 13:51:37 -0300

    Add files via upload

---
+++ src/js/links.js
@@ -0,0 +1,12 @@
+// SOCIALS
+
+const steam = () => (window.open("https://steamcommunity.com/id/apoiador/"));
+const instagram = () => (window.open("https://instagram.com/d3vilgothic"));
+const github = () => (window.open("https://github.com/bielwtf"));
+const spotify = () => (window.open("https://open.spotify.com/user/31blxdx72usfb4srh6f5ppq6zhom?si=78875c1cf1824c5c"));
+const pinterest = () => (window.open("https://br.pinterest.com/devilgohies/"));
+
+// OTHERS
+
+const arch = () => (window.open("https://discord.gg/cassian"));
+const biel = () => (window.open("https://discord.gg/cassian"));
